aadf16240091667f6b5d5233cd1c017caa7ef347
Tron <edunhenry@gmail.com>
2024-07-22 11:53:01 +0100

    added new method for bfs and commented the previous one

--- hackerrankgraphproblem1st.js
+++ hackerrankgraphproblem1st.js
@@ -13,61 +13,85 @@ addEdge(v1, v2) {
     this.adjacencyList[v2].push(v1);
 }
 
-bfs(start_node,val,ids) {
-    const queue = [start_node];
+// bfs(start_node,val,ids) {
+//     const queue = [start_node];
+//
+//     /* when checking the start_node has been visited
+//     also check the color of the start_node from the
+//     ids array to see if it is the color_i_want (val)
+//     when I see the color_i_want two times exit the loop
+//     and return the distance counter or -1 if not color
+//     was found
+//      */
+//
+//     /* if on node 4 check ids at index 4 (ids[4]) to see if it is the color_i_need (val)
+//        if on node 0 check ids at index 0 (ids[0]) to see if it is the color_i_need (val)
+//        if on node 8 check ids at index 8 (ids[0]) to see if it is the color_i_need (val)
+//      */
+//
+//     const result = [];
+//
+//     const color_i_need = val;
+//     const color_arr = ids;
+//
+//     let color_i_need_count = 0;
+//     // when the color_i_need_count is two return the counter
+//
+//     let distance_counter = 0;
+//     /* counting the weight from the  color_i_need at the start to the color
+//      I need at the end */
+//
+//     const visited = {};
+//     visited[start_node] = true;
+//
+//     while (queue.length !== 0) {
+//         let currentVertex = queue.shift();
+//         /* The shift() method removes the first item of an array.
+//         The shift() method changes the original array.
+//          The shift() method returns the shifted element.
+//          */
+//
+//         result.push(currentVertex);
+//
+//         /* The push() method adds new items to the end of an array.
+//          The push() method changes the length of the array.
+//          */
+//
+//         this.adjacencyList[currentVertex].forEach(neighbor => {
+//             if (!visited[neighbor]) {
+//                 visited[neighbor] = true;
+//                 queue.push(neighbor);
+//             }
+//         });
+//
+//         /* so I have to change the iterative loop to a for loop that iterates from an index 0 to array.length */
+//     }
+//     return result;
+// }
 
-    /* when checking the start_node has been visited
-    also check the color of the start_node from the
-    ids array to see if it is the color_i_want (val)
-    when I see the color_i_want two times exit the loop
-    and return the distance counter or -1 if not color
-    was found
-     */
+    // Prints BFS traversal from all unvisited vertices
+    BFS() {
+        let visited = new Array(this.V).fill(false);
 
-    /* if on node 4 check ids at index 4 (ids[4]) to see if it is the color_i_need (val)
-       if on node 0 check ids at index 0 (ids[0]) to see if it is the color_i_need (val)
-       if on node 8 check ids at index 8 (ids[0]) to see if it is the color_i_need (val)
-     */
+        for (let i = 0; i < this.V; ++i) {
+            if (!visited[i]) {
+                let queue = [];
+                visited[i] = true;
+                queue.push(i);
 
-    const result = [];
-
-    const color_i_need = val;
-    const color_arr = ids;
-
-    let color_i_need_count = 0;
-    // when the color_i_need_count is two return the counter
-
-    let distance_counter = 0;
-    /* counting the weight from the  color_i_need at the start to the color
-     I need at the end */
-
-    const visited = {};
-    visited[start_node] = true;
-
-    while (queue.length !== 0) {
-        let currentVertex = queue.shift();
-        /* The shift() method removes the first item of an array.
-        The shift() method changes the original array.
-         The shift() method returns the shifted element.
-         */
-
-        result.push(currentVertex);
-
-        /* The push() method adds new items to the end of an array.
-         The push() method changes the length of the array.
-         */
-
-        this.adjacencyList[currentVertex].forEach(neighbor => {
-            if (!visited[neighbor]) {
-                visited[neighbor] = true;
-                queue.push(neighbor);
+                while (queue.length !== 0) {
+                    let s = queue.shift();
+                    process.stdout.write(s + " ");
+                    for (let n of this.adj[s]) {
+                        if (!visited[n]) {
+                            visited[n] = true;
+                            queue.push(n);
+                        }
+                    }
+                }
             }
-        });
-
-        /* so I have to change the iterative loop to a for loop that iterates from an index 0 to array.length */
+        }
     }
-    return result;
-}
 }
 
 // !!! make any mistake you can until there are no more mistakes
